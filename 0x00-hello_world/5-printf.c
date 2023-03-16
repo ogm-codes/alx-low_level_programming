ce57a1c395af919d909b2b3ccffad421bd062d34
Oratile <og.mogorosi28@gmail.com>
2023-03-16 11:29:55 -0700

    Printf commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+int main()
+{
+	printf("with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
