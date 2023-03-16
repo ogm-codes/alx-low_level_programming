ceed95e1c52b93e1fbd5c3875b5c1b726c276458
Oratile <og.mogorosi28@gmail.com>
2023-03-16 11:18:11 -0700

    Betty error fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
- * main - start of the program
- * function_puts - prints out a string
- * return: 0 to end the program
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
  */
 int main(void)
 {
