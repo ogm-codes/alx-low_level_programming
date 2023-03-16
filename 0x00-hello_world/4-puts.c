4548b30f78eda25adc1ca82cf939b3ae7fe469ba
Oratile <og.mogorosi28@gmail.com>
2023-03-16 10:32:42 -0700

    C file commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*
+ * main - start of the program
+ * function_puts - prints out a string
+ * return: 0 to end the program
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
