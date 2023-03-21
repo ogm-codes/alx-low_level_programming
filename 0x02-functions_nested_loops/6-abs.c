816c57a0a10e33fb85b0e69398ea9f26e0beee01
Oratile <og.mogorosi28@gmail.com>
2023-03-21 03:11:09 -0700

    number 6 commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,8 +1,11 @@
 #include "main.h"
 
 /**
- *
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
  */
+
 int _abs(int)
 {
 	if (n >= 0)
