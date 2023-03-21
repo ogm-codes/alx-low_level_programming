2f1664601a2ab96d8f2935d3084b15be5ebf6385
Oratile <og.mogorosi28@gmail.com>
2023-03-21 09:24:58 -0700

    Task 10 and 11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - check code
+ * @a: int added to b
+ * @b: int added to a
+ * Return: the sum of a and b
+ */
+int add(int a, int b)
+{
+	int z;
+
+	z = a + b;
+
+	return (z);
+}
