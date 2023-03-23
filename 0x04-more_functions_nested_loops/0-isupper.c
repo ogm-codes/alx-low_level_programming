f5616fe9727c9b47d746daf86ac653007bed603e
Oratile <og.mogorosi28@gmail.com>
2023-03-23 10:45:29 -0700

    04 first commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - determines case sensitivity
+ *
+ * Return: 1 if uppercase, 0 therwise
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		Return (1);
+	}
+	else
+	{
+		Return (0);
+	}
+}
