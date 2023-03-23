1a23c7dc797f1382b7bfc0481870ccab4a47eae6
Oratile <og.mogorosi28@gmail.com>
2023-03-23 10:58:27 -0700

    Second commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
  * _isupper - determines case sensitivity
- *
+ *@c: character to check
+
  * Return: 1 if uppercase, 0 therwise
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
 	{
-		Return (1);
+		Return(1);
 	}
 	else
 	{
-		Return (0);
+		Return(0);
 	}
 }
