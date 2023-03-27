e0fcf1f6561a4c6d91a7e8fa352e6bf2fdaa05c1
Oratile <og.mogorosi28@gmail.com>
2023-03-27 13:35:13 -0700

    num 4 and 5

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,14 +9,14 @@ void print_rev(char *s)
 {
 	int a = 0;
 
-	while (s[c] != '\0')
+	while (s[a] != '\0')
 	{
 		a++;
 	}
 
 	for (a -= 1; a >= 0; a--)
 	{
-		_putchar(s[c]);
+		_putchar(s[a]);
 	}
 
 	_putchar('\n');
---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - prints string or word in reverse
+ * @s: calculates each individual string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	char placeHolder;
+	int length = 0, counter = 0;
+
+	while (s[counter++])
+	{
+		length++;
+	}
+
+	for (counter = length - 1; counter >= length / 2; counter--)
+	{
+		placeholder = s[counter];
+		s[counter] = s[length - counter - 1];
+		s[length - counter - 1] = placeholder;
+	}
+}
