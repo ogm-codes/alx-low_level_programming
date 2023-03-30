b19f216c19526b18a70bbfc10db80ca2616b7cd9
Oratile <og.mogorosi28@gmail.com>
2023-03-30 15:22:50 -0700

    Task 6 fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,6 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-		{
 			index++;
 			if (str[index - 1] == ' ' || str[index - 1] == '\t'
 			|| str[index - 1] == '\n' || str[index - 1] == ','
@@ -20,11 +19,8 @@ char *cap_string(char *str)
 			|| str[index - 1] == '"' || str[index - 1] == '('
 			|| str[index - 1] == ')' || str[index - 1] == '{'
 			|| str[index - 1] == '}' || index == 0)
-			{
 				str[index] -= 32;
 				index++;
-			}
-		}
 	}
 	return (str);
 }
