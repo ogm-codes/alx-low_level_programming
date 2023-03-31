340929024851fce3028275d0d170b1fd0bbe1f00
Oratile <og.mogorosi28@gmail.com>
2023-03-30 17:17:00 -0700

    Last

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,7 +11,9 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		{
 			index++;
+
 			if (str[index - 1] == ' ' ||
 			str[index - 1] == '\t' ||
 			str[index - 1] == '\n' ||
@@ -26,8 +28,11 @@ char *cap_string(char *str)
 			str[index - 1] == '{' ||
 			str[index - 1] == '}' ||
 			index == 0)
+			{
 				str[index] -= 32;
 				index++;
+			}
+		}
 	}
 	return (str);
 }
