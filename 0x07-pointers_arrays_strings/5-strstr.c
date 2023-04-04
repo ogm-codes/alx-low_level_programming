d7dbf93ec66c33b2e05d78d4b229417d96ab739b
Oratile <og.mogorosi28@gmail.com>
2023-04-03 17:40:49 -0700

    Task fixes logic fixes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,6 +7,21 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int a;
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *first = haystack;
+		char *scnd = needle;
 
+		while (*first == *scnd && *scnd != '\0')
+		{
+			first++;
+			scnd++;
+		}
+
+		if (*scnd == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
 }
