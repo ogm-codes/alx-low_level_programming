527523fbe5431ec1170efbf9aa6f4c31441db9f4
Oratile <og.mogorosi28@gmail.com>
2023-04-05 13:14:20 -0700

    Task 0 fix

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns length of a string
+ * @s: pointer
+ * Return: returns a number which is length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int count;
+
+	if (*s)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
