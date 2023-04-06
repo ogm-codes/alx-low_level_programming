f2991c9893198fd858cb27c32f701a3364c42ad9
Oratile <og.mogorosi28@gmail.com>
2023-04-06 03:57:25 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,12 +6,30 @@
  */
 int is_prime_number(int n)
 {
-	if (n % 2 == 0)
-	{
-		return (1);
-	}
-	else
+	return (check(n, 1));
+}
+/**
+ * check - determine if x is prime
+ * @x: number to be checked
+ * @y: the mod% number
+ * Return: 1 if prime otherwise 0
+ */
+int check(int x, int y)
+{
+	if (x <= 1)
 	{
 		return (0);
 	}
+
+	if (x % y == 0 && y > 1)
+	{
+		return (0);
+	}
+
+	if ((x / y) < y)
+	{
+		return (1);
+	}
+
+	return (check(x, y + 1));
 }
