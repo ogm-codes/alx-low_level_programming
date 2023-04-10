5979968c20b09407a609a74095a7444d5aa62420
Oratile <og.mogorosi28@gmail.com>
2023-04-10 10:27:32 -0700

    First commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - pritns all argument recieved
+ * @argc: counts the arguments
+ * @argv: pointer array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	for (int a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
