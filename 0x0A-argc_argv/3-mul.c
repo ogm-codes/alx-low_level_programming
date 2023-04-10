2ad7ed38ba386da4363a5171a569f208c458240f
Oratile <og.mogorosi28@gmail.com>
2023-04-10 10:45:20 -0700

    Second commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.>
+/**
+ * main - multiplies two numbers
+ * @argc: counts the arguments
+ * @argv: pointer array
+ * Return: 0, otherwise 1 if there is an erro
+ */
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+
+	if (argc == 3)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
