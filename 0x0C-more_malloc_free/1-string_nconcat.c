4578e2bafcda64e64328b6beba65493569742b9f
Oratile <og.mogorosi28@gmail.com>
2023-04-13 17:25:11 -0700

    task 2

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes
+ * Return: Null on failure
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int a = 0, b = 0, c = 0, d = 0;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[a])
+		a++;
+
+	while (s2[b])
+		b++;
+
+	if (n >= b)
+		d = a + b;
+	else
+		d = a + n;
+
+	s = malloc(sizeof(char) * d + 1);
+
+	if (s == NULL)
+		return (NULL);
+
+	c = 0;
+	while (c < d)
+	{
+		if (b < a)
+			s[b] = s1[b];
+
+		if (b >= a)
+		{
+			s[b] = s2[c];
+			c++;
+		}
+		b++;
+	}
+	s[b] = '\0';
+	return (s);
+}
+
