fda34dd1334eae3b408709bb52a4eefff8adddf6
Oratile <og.mogorosi28@gmail.com>
2023-04-14 15:57:01 -0700

    Task 2 fix

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc -
+ * @nmemb: element of array
+ * @size: bytes
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int i = 0; c = 0;
+	char *arr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	c = nmemb * size;
+	arr = malloc(c);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	while (i < c)
+	{
+		arr[i] = 0;
+		i++;
+	}
+
+	return (arr);
+}
