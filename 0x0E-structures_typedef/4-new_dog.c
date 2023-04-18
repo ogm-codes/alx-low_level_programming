a7f0b77af3ad790d7a2b7658e4034829453e9573
Oratile <og.mogorosi28@gmail.com>
2023-04-18 13:34:49 -0700

    task 4 fix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -59,12 +59,12 @@ char *_strcpy(char *dest, char *src)
 {
 	int a;
 
-	for (a = 0; src[i] != '\0'; i++)
+	for (a = 0; src[a] != '\0'; a++)
 	{
-		dest[i] = src[i];
+		dest[a] = src[a];
 	}
 
-	dest[i++] = '\0';
+	dest[a++] = '\0';
 
 	return (dest);
 }
