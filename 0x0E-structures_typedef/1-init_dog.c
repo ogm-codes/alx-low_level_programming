b0a74462fc18983e4e61dca722e43e2512235365
Oratile <og.mogorosi28@gmail.com>
2023-04-18 10:16:02 -0700

    task 5

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,1 +1,19 @@
-
+#include "dog.h"
+/**
+ * init_dog - initialises dog struct
+ * @d: dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
