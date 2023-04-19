2a45dea9bbd4754742364c004f5d24b674c98e4f
Oratile <og.mogorosi28@gmail.com>
2023-04-19 14:43:05 -0700

    task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -16,11 +16,7 @@ int op_add(int a, int b)
  */
 int op_sub(int a, int b)
 {
-	if (b > a)
-		return (b - a);
-
-	if (a > b)
-		return (a - b);
+	return (a - b);
 }
 /**
  * op_mul - multiply a with b
