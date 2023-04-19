4722cbde5c27defda7e236f010d019d421c6619b
Oratile <og.mogorosi28@gmail.com>
2023-04-19 14:26:26 -0700

    task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+/**
+ * op_add - addition
+ * @a: number
+ * @b: number
+ * Return: numbers added
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction
+ * @a: number
+ * @b: number
+ * Return: numbers subtracted
+ */
+int op_sub(int a, int b)
+{
+	if (b > a)
+		return (b - a);
+
+	if (a > b)
+		return (a - b);
+}
+/**
+ * op_mul - multiply a with b
+ * @a: number
+ * @b: number
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: number
+ * @b: number
+ * Return: a/b
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
+}
+/**
+ * op_mod - modulus
+ * @a: number
+ * @b: number
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a % b);
+}
