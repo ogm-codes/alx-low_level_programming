480922cc95c05cd6f712eed0cba27016692ffec0
Rati <og.mogorosi28@gmail.com>
2023-06-29 12:33:04 -0700

    --task_fix

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+
+void __attribute__((constructor)) hare(void);
+
+/**
+ * hare - prints string before main executes
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -33,7 +33,7 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new->str = duplicate;
 	new->len = length;
-	new->next = *head;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
