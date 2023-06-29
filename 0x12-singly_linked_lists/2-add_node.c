51c88414779d40f4947ebce749fb189002d59488
Rati <og.mogorosi28@gmail.com>
2023-06-29 09:02:52 -0700

    --files_update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,9 +1,44 @@
+#include <string.h>
 #include "lists.h"
 
 /**
- *
+ * add_node -
+ * @head:
+ * @str
+ * Return:
  */
 list_t *add_node(list_t **head, const char *str)
 {
+	char *duplicate;
+	list_t *new;
+	int length;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	duplicate = strdup(str);
+
+	if (duplicate == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (length = 0; str[length];)
+	{
+		length++;
+	}
+
+	new->str = duplicate;
+	new->length = length;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
 }
 
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include "list.h"
+
+/**
+ *
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *last;
+	char *duplicate;
+}
